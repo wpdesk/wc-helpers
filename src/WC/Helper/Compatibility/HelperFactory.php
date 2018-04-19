4d53bd6152aad98d72c6ea42c6c770826a4bd5fb
dyszczo <krzysztof.dyszczyk@gmail.com>
2018-04-19 17:09:59 +0200

    product compatible interface

--- src/WC/Helper/Compatibility/HelperFactory.php
+++ src/WC/Helper/Compatibility/HelperFactory.php
@@ -2,8 +2,22 @@
 
 namespace WPDesk\WC\Helper\Compatibility;
 
+use WPDesk\WC\Helper\Order\OrderCompatible;
+use WPDesk\WC\Helper\Product\ProductCompatible;
+
 interface HelperFactory
 {
+    /**
+     * @param \WC_Product $product
+     *
+     * @return ProductCompatible
+     */
     public function create_product_helper( \WC_Product $product );
+
+    /**
+     * @param \WC_Product $product
+     *
+     * @return OrderCompatible
+     */
     public function create_order_helper( \WC_Order $order );
 }
--- src/WC/Helper/Compatibility/HelperFactoryLastest.php
+++ src/WC/Helper/Compatibility/HelperFactoryLastest.php
@@ -2,13 +2,26 @@
 
 namespace WPDesk\WC\Helper\Compatibility;
 
+use WPDesk\WC\Helper\Order\OrderCompatible;
+use WPDesk\WC\Helper\Product\ProductCompatible;
+
 class HelperFactoryLastest implements HelperFactory
 {
+    /**
+     * @param \WC_Product $product
+     *
+     * @return ProductCompatible
+     */
     public function create_product_helper(\WC_Product $product)
     {
         return new \WPDesk\WC\Helper\Product\Compatible\Latest($product);
     }
 
+    /**
+     * @param \WC_Product $product
+     *
+     * @return OrderCompatible
+     */
     public function create_order_helper(\WC_Order $order)
     {
         return new \WPDesk\WC\Helper\Order\Compatible\Latest($order);
